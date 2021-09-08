f26cd51e44c0f88bcddad50d90a05a47a9f9c249
santosh <santoshvkawale123@gmail.com>
2021-09-08 13:34:48 +0530

    Thhis is my first project
    this is first project

---
+++ Velocitydemo/src/mayevening/Demoproject.java
@@ -0,0 +1,11 @@
+package mayevening;
+
+public class Demoproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is demo project");	
+
+	}
+
+}
